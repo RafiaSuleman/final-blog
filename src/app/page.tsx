8539a659be9a55e35a1f1d6a528d4ef1de3687fb
RafiaSuleman <rafiasuleman47@gmail.com>
2024-09-26 22:46:40 +0500

    footerfixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ import { ArrowRight } from "lucide-react";
 import { format, parseISO } from "date-fns";
 import { sortBlogs } from "../utils";
 import { Button } from "@/components/ui/button";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   const blogs = sortBlogs(allBlogs);
@@ -24,7 +27,7 @@ export default function Home() {
           <div className="flex gap-3 "><div className="border-4 border-[#4c1d95]"></div>
           <div >
           <p>
-          Stay updated with the latest insights, trends, and tips on Technology. Our curated articles provide valuable resources to inspire, educate, and keep you informed. Whether you're a seasoned expert or just starting, you'll find something useful to explore. Dive into our most recent posts and stay connected to what matters.
+          Stay updated with the latest insights, trends, and tips on Technology. Our curated articles provide valuable resources to inspire, educate, and keep you informed. Whether you are a seasoned expert or just starting, you will find something useful to explore. Dive into our most recent posts and stay connected to what matters.
           </p>
          
           </div>
@@ -126,97 +129,10 @@ export default function Home() {
           
           </div>
 
-          {/* cards */}
-          {/* <div className="mx-auto mt-8 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3 ">
-            {blogs.map((blog: any, index: any) => {
-              return (
-                <div className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 dark:bg-gray-700 px-8 py-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-                  <img
-                    src={blog.image.filePath.replace("../public", "")}
-                    alt={blog.authorName}
-                    className="absolute inset-0 -z-10 h-full "
-                  />
-
-                  <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-                  <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
-                  <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-                    {blog.author}{" "}
-                    {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
-                  </div>
-
-                  {index === 0 && ( // Assuming the first blog is the latest blog
-                    <div className="absolute top-0 right-0">
-                      <div className="w-32 h-8 absolute top-4 -right-4">
-                        <div className="h-full w-full bg-red-500 text-white text-center leading-8 font-semibold ">
-                          Latest Blog
-                        </div>
-                      </div>
-                    </div>
-                  )}
-
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
-                    <a href={blog.url}>
-                      <span className="absolute inset-0"></span> {blog.title}
-                    </a>
-                  </h3>
-                </div>
-              );
-            })}
-          </div> */}
+      
         </div>
       </div>
-      {/* <div className="relative bg-gray-50 pb-20  lg:pb-28 ">
-        <div className="absolute inset-0">
-          <div className="h-1/3 bg-white sm:h-2/3"></div>
-        </div>
-        <div className="relative ">
-          <div className=" grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3 md:grid-cols-2 align-content:space-between ">
-            {blogs.map((blog: any, index: any) => {
-              return (
-                <div
-                  className="flex flex-col overflow-hidden rounded-lg shadow-lg"
-                  key={index}
-                >
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-48 w-full object-cover"
-                      src={blog.image.filePath.replace("../public", "")}
-                      alt={blog.authorName}
-                    />
-                  </div>
-                  <div className="flex flex-1 flex-col justify-between bg-white p-6">
-                    <div className="flex-1">
-                      <h1 className="text-semibild text-sm">
-                        {blog.author}{" "}
-                        {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
-                      </h1>
-                      <Link href={blog.url} className="mt-2 block">
-                        <div className="flex gap-5 justify-between text-xl font-semibold text-gray-900">
-                          <h1 className="w-[90%]"> {blog.title} </h1>
-                          <ArrowRight className="max-w-10" rotate={45} />
-                        </div>
-                        <p className="mt-3 text-base text-gray-500 ">
-                        {truncateText(blog.description, 20)}
-                        </p>
-                      </Link>
-                    </div>
-                    <div className="mt-6 flex items-center gap-2 text-sm">
-                      <div className="border border-black px-2 py-1 rounded-full ">
-                      {blog.tags[2]}
-                      </div>
-                      <div className="border border-black px-2 py-1 rounded-full ">
-                        {blog.tags[1]}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </div> */}
-
-      {/* paggination */}
+     
     </div>
   );
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,39 +1,74 @@
 import Link from "next/link";
 import React from "react";
-import { FaSquareFacebook } from "react-icons/fa6";
+import Image from "next/image";
+import { FaLinkedin, FaSquareFacebook } from "react-icons/fa6";
 import { FaWhatsappSquare } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { CgMail } from "react-icons/cg";
-import { FaPhoneAlt } from "react-icons/fa";
-import { MdLocationOn } from "react-icons/md";
+
 const Footer = () => {
   const Links = [
     {
-      name: "Home",
+      name: "Blog",
       path: "/",
     },
     {
-      name: "Categories",
-      path: "/categories/all",
+      name: "Careers",
+      path: "/",
     },
 
     {
-      name: "Popular Blogs",
-      path: "/popularblogs",
+      name: "Pricing",
+      path: "/",
+    },
+  ];
+  const Links1 = [
+    {
+      name: "Documentations",
+      path: "/",
+    },
+    {
+      name: "Papers",
+      path: "/",
+    },
+
+    {
+      name: "Press Conferences",
+      path: "/",
+    },
+  ];
+  const Links2 = [
+    {
+      name: "Terms of Service",
+      path: "/",
+    },
+    {
+      name: "Privacy Policy",
+      path: "/",
+    },
+
+    {
+      name: "Cookies Policy",
+      path: "/",
+    },
+    {
+      name: "Data Processing",
+      path: "/",
     },
   ];
 
   return (
     <footer className="w-full bg-gray-900">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-3">
-        <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-8 gap-12 pt-14 pb-20 max-w-md mx-auto md:max-w-xl lg:max-w-full text-white">
-          <div>
-            <h1 className="font-medium mb-5">Tech Blog</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="grid lg:grid-cols-2 grid-cols-1  text-sm text-white">
+        <div>
+         
+            <h1 className="mt-8 font-medium mb-5">TechHives</h1>
+            {/* <p>Explore diverse topics with expert insights, from technology and business to lifestyle and creativity. Stay informed and inspired with our latest blogs.</p> */}
+            <p>Diverse expert insights across topics.</p>
           </div>
+          <div className="grid xl:grid-cols-3 grid-cols-2 pt-14 pb-10  lg:max-w-full text-white gap-5">
+         
           <div>
-            <h1 className="mb-4 font-medium">Quick Links</h1>
+            <h1 className="mb-4 font-medium">COMPANY</h1>
             {Links.map((link, index) => {
               return (
                 <div key={index}>
@@ -43,63 +78,58 @@ const Footer = () => {
             })}
           </div>
           <div>
-            <h1 className="mb-5 font-medium">Contact Us</h1>
-            <div className="flex gap-3 justify-start items-center ">
-            <CgMail className="w-8 h-8"/>rafiakhurshid00@gmail.com
-            </div>
-          <div className="flex gap-3 justify-start items-center">
-            <FaPhoneAlt className="w-8 h-5" />Phone Number
-          </div>
-          <div className="flex gap-3 justify-start items-center">
-            <MdLocationOn  className="w-8 h-8" /> Location
-          </div>
-    
-
+            <h1 className="mb-4">RESOURCES</h1>
+            {Links1.map((link, index) => {
+              return (
+                <div key={index}>
+                  <Link href={link.path}>{link.name}</Link>
+                </div>
+              );
+            })}
           </div>
           <div>
-            <h1 className="mb-5 font-medium">Follow us</h1>
-            <div className="flex  gap-3">
-              <div>
-                <Link
+            <h1 className=" font-medium ">LEGAL</h1>
+            {Links2.map((link, index) => {
+              return (
+                <div key={index}>
+                  <Link href={link.path}>{link.name}</Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        </div>
+       
+        <div className="py-7 border-t border-gray-700 ">
+          <div className="flex flex-col xl:justify-between mx-auto items-center xl:flex-row text-white">
+            <div > <p>©2024techHives All rights reserved</p></div>
+            
+            <div className="flex gap-3 text-right ml-auto">
+            <Link
                   href={"https://www.facebook.com/sheza.siddiqui1"}
                   target="_blank"
                 >
-                  <FaSquareFacebook className="w-10 h-10 hover:-rotate-12" />
+                  <FaSquareFacebook className="w-7 h-10 hover:-rotate-12" />
                 </Link>
-              </div>
-              <div>
-                <Link
-                  href={"https://www.facebook.com/sheza.siddiqui1"}
-                  target="_blank"
-                >
-                  <FaWhatsappSquare className="w-10 h-10 hover:-rotate-12" />
-                </Link>
-              </div>
-              <div>
-                <Link
-                  href={"https://www.facebook.com/sheza.siddiqui1"}
-                  target="_blank"
-                >
-                  <FaTwitter className="w-10 h-10 hover:-rotate-12" />
-                </Link>
-              </div>
-              <div>
                 <Link
                   href={
                     "https://www.linkedin.com/in/rafia-khurshid-853510b7/recent-activity/all/"
                   }
                   target="_blank"
                 >
-                  <FaLinkedin className="w-10 h-10 hover:-rotate-12" />
+                  <FaLinkedin className="w-7 h-10  hover:-rotate-12" />
                 </Link>
-              </div>
+                <Link
+                  href={"https://www.facebook.com/sheza.siddiqui1"}
+                  target="_blank"
+                >
+                  <FaWhatsappSquare className="w-7 h-10  hover:-rotate-12" />
+                </Link>
             </div>
-          </div>
-        </div>
-        <div className="py-7 border-t border-gray-700 ">
-          <div className="flex justify-center flex-col items-center lg:space-y-0 xl:justify-between xl:flex-row text-white">
-            <p>TecHives</p>
-            <p>2024TechHives all rights reserved</p>
+           
+            <p>
+
+            </p>
           </div>
         </div>
       </div>
